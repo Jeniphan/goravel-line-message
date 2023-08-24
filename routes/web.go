fc2e675f207c09137f8037df7f9ac6668322949f
akiia-axons <jetniphan.puk@axonstech.com>
2023-08-24 15:05:13 +0700

    update prefix to linechat

--- routes/web.go
+++ routes/web.go
@@ -20,23 +20,27 @@ import (
 func Web() {
 
 	userController := controllers.NewUserController()
-	facades.Route().Get("/", userController.Show)
 
 	lineController := line.NewLineController()
-	facades.Route().Prefix("message").Group(func(route route.Route) {
-		route.Post("/webhook", lineController.LineWebhookHandler)
-		route.Post("/createLineConfig", lineController.CreateLineConfig)
-		route.Put("/updateLineConfig", lineController.UpdateLineConfig)
+	authController := line_auth.NewLineAuth()
+
+	facades.Route().Prefix("linechat").Group(func(mainRoute route.Route) {
+		mainRoute.Prefix("message").Group(func(route route.Route) {
+			route.Get("/", userController.Show)
+			route.Post("/webhook", lineController.LineWebhookHandler)
+			route.Post("/createLineConfig", lineController.CreateLineConfig)
+			route.Put("/updateLineConfig", lineController.UpdateLineConfig)
+		})
+
+		mainRoute.Prefix("auth").Group(func(routeApiInGroup route.Route) {
+			routeApiInGroup.Get("/", authController.LineLogin)
+			routeApiInGroup.Get("/callback", authController.LineLoginCallback)
+			routeApiInGroup.Get("/refresh", authController.LineRefresh)
+			routeApiInGroup.Get("/profile", authController.LineLoginProfile)
+			routeApiInGroup.Get("/revoke", authController.LineRevoke)
+			routeApiInGroup.Get("/verify", authController.LineVerify)
+		})
 	})
 
-	authController := line_auth.NewLineAuth()
 	// facades.Route().Get("/auth/line", authController.LineLogin)
-	facades.Route().Prefix("auth/line").Group(func(routeApiInGroup route.Route) {
-		routeApiInGroup.Get("/", authController.LineLogin)
-		routeApiInGroup.Get("/callback", authController.LineLoginCallback)
-		routeApiInGroup.Get("/refresh", authController.LineRefresh)
-		routeApiInGroup.Get("/profile", authController.LineLoginProfile)
-		routeApiInGroup.Get("/revoke", authController.LineRevoke)
-		routeApiInGroup.Get("/verify", authController.LineVerify)
-	})
 }
